2cc65e98418955a8f40b96bf780e2d983f0f34ae
RahimliZakir <zakirer@code.edu.az>
2022-02-13 05:17:04 +0400

    Films Swiper Side Done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
   $("header").load("_header.html");
+
+  let filmsSwiperCarousel = new Swiper(".films-swiper-carousel", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+  });
+
+  let filmsSwiperItem = $(".films-swiper-item");
+
+  $.each(filmsSwiperItem, (index, item) => {
+    let bgThumb = $(item).attr("bg-thumb");
+
+    $(item).css(
+      "background-image",
+      `linear-gradient(to top, #1e2129, transparent), url(assets/images/home/${bgThumb})`
+    );
+  });
 });
