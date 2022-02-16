c04f5a86e16fe88d6573b8b6a4d576f0f32e8f5b
RahimliZakir <zakirer@code.edu.az>
2022-02-17 00:27:23 +0400

    Contact Us Side Static Side Written

--- assets/js/contactus.js
+++ assets/js/contactus.js
@@ -1,4 +1,19 @@
 $(function () {
   $("header").load("_header.html");
   $("footer").load("_footer.html");
+
+  let contactFormCols = $("#contact-side-form>[class*='col']");
+  let contactFormLabels = $(contactFormCols).find("label");
+
+  $.each(contactFormLabels, (index, item) => {
+    let formElement = $(item).next();
+    let requiredPlaceholder = $(item).next().attr("placeholder");
+
+    if (
+      $(item).hasClass("label-required") &&
+      $(requiredPlaceholder) != undefined
+    ) {
+      $(formElement).attr("placeholder", `${requiredPlaceholder}*`);
+    }
+  });
 });
