a4221a878ad24b5c5495a822d6afb2c41ce5c6aa
RahimliZakir <zakirer@code.edu.az>
2022-02-26 22:54:53 +0400

    Wishlist Done

--- assets/js/wishlist.js
+++ assets/js/wishlist.js
@@ -15,8 +15,36 @@ $(document).ready(function () {
     $(wishlistShowsRow).addClass("visible");
   }
 
-  let wishlistMoviesCards = $(wishlistMoviesRow).children();
-  let wishlistShowsCards = $(wishlistShowsRow).children();
+  let wishlistMoviesCards = $(wishlistMoviesRow).children().find(".card");
+  let wishlistShowsCards = $(wishlistShowsRow).children().find(".card");
+
+  let wishlistMoviesImgDivs = $(wishlistMoviesCards).find(".img-div");
+  let wishlistShowsImgDivs = $(wishlistShowsCards).find(".img-div");
+  $.each(wishlistMoviesImgDivs, function (index, item) {
+    let timesIcon = $("<i/>", {
+      class: "bi bi-x-circle remove-movie-btn",
+      style:
+        "cursor: pointer;position: absolute;right: 5px;bottom: 5px;color: #fff;z-index: 2;",
+    });
+    $(item).prepend(timesIcon);
+  });
+  $.each(wishlistShowsImgDivs, function (index, item) {
+    let timesIcon = $("<i/>", {
+      class: "bi bi-x-circle remove-show-btn",
+      style:
+        "cursor: pointer;position: absolute;right: 5px;bottom: 5px;color: #fff;z-index: 2;",
+    });
+    $(item).prepend(timesIcon);
+  });
+
+  let moviesTimesIcons = $(wishlistMoviesCards).find(".remove-movie-btn");
+  $(moviesTimesIcons).click(function () {
+    $(this).closest(".latest-movies-card-col").remove();
+  });
+  let showsTimesIcons = $(wishlistShowsCards).find(".remove-show-btn");
+  $(showsTimesIcons).click(function () {
+    $(this).closest(".latest-tv-show-bottom-col").remove();
+  });
 
   $(wishlistFilterMoviesBtn).on("click", (e) => {
     $(wishlistShowsRow).removeClass("visible");
