5fda39f0885817cede93925c6e222d837abab97b
RahimliZakir <zakirer@code.edu.az>
2022-03-04 01:17:52 +0400

    Star Ratings Edited

--- assets/js/movie-details.js
+++ assets/js/movie-details.js
@@ -4,25 +4,9 @@ $(document).ready(function () {
 
   let stars = $(".star-rating-div>i");
 
-  $(stars).on("mouseenter", function () {
+  $(stars).on("click", function () {
+    $(stars).removeClass("fas").addClass("far");
     $(this).removeClass("far").addClass("fas");
     $(this).prevAll("i").removeClass("far").addClass("fas");
-  });
-
-  let clickHandler = false;
-  
-  $(stars).on("mouseleave", function (e) {
-    if (!clickHandler) {
-      $(this).removeClass("fas").addClass("far");
-      $(this).prevAll("i").removeClass("fas").addClass("far");
-    } else {
-      clickHandler = false;
-    }
-  });
-
-  $(stars).click((e) => {
-    $(this).removeClass("far").addClass("fas");
-    $(this).prevAll("i").removeClass("far").addClass("fas");
-    clickHandler = true;
   });
 });
